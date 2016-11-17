020ad03631dd42f28dfd07459e5ebae1a8451b42
ubapook2 <n02478248@hawkmail.newpaltz.edu>
2016-11-17 18:17:19 -0500

    3 types of enemy movement (full)

---
+++ Assets/Scripts/PatrolMovement.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PatrolMovement : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    Vector3 direction = Vector3.left;
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        transform.position += direction * Time.deltaTime;
+
+        if (transform.position.x <= -1)
+        {
+            direction = new Vector3(1, .3f);
+        }
+        if (transform.position.x >= 6)
+        {
+            direction = new Vector3(-1, -.3f);
+        }
+
+    }
+
+}
